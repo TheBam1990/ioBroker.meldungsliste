2d407d2c8c509d512790915f57d930752786349e
TheBam1990 <ulf2356@web.de>
2020-04-04 18:53:34 +0200

    io package geändert

--- admin/words.js
+++ admin/words.js
@@ -14,28 +14,26 @@ systemDictionary = {
 		"pl": "Ustawienia adaptera dla meldungsliste",
 		"zh-cn": "meldungsliste的适配器设置"
 	},
-	"option1": {
-		"en": "option1",
-		"de": "Option 1",
-		"ru": "Опция 1",
-		"pt": "Opção 1",
-		"nl": "Optie 1",
-		"fr": "Option 1",
-		"it": "opzione 1",
-		"es": "Opción 1",
-		"pl": "opcja 1",
-		"zh-cn": "选项1"
-	},
-	"option2": {
-		"en": "option2",
-		"de": "Option 2",
-		"ru": "option2",
-		"pt": "opção 2",
-		"nl": "Optie 2",
-		"fr": "Option 2",
-		"it": "opzione 2",
-		"es": "opcion 2",
-		"pl": "Opcja 2",
-		"zh-cn": "选项2"
-	}
+   "PING adapter settings": {"en": "PING adapter settings", "de": "Listen Adapter-Einstellungen", 			"ru": "Настройки драйвера PING"},
+    "List settings":         {"en": "List settings",         "de": "Meldungslisten Einstellungen",         "ru": "Настройки PING"},
+    "Ping addresses":        {"en": "IP addresses to ping",  "de": "Liste der Meldungen", 					 "ru": "Список IP адресов для мониторинга"},
+    "Interval[ms]:":         {"en": "Ping interval[ms]",     "de": "Abfrage Interval [ms]",       			  "ru": "Интервал опроса[мс]:"},
+    "Room":                  {"en": "Room",                  "de": "Meldungstext",                   	    "ru": "Комната"},
+    "Name":                  {"en": "Name",                  "de": "Objekte",                     			  "ru": "Имя"},
+    "IF Bedingung":            {"en": "IP Address",            "de": "IF Bedingung",               			  "ru": "IP Адрес"},
+    "No host name in state name:": {
+        "en": "No host name in state name:",
+        "de": "Kein Hostnamen im Zustandsnamen:",
+        "ru": "Имена состояний без имени хоста:"
+    },
+    "tooltip_interval": {
+        "en": "Polling interval in ms",
+        "de": "Abfrage Intervall in ms",
+        "ru": "Интервал опроса в миллисекундах"
+    },
+    "tooltip_noHostname": {
+        "en": "Do not include the host name into state's names: ping.0.127_0_0_1 / ping.0.host.127_0_0_1",
+        "de": "Inkludiere kein Hostnamen in die Zustandsnamen:  ping.0.127_0_0_1 / ping.0.host.127_0_0_1",
+        "ru": "Убрать имя хоста из имён состояний: ping.0.127_0_0_1 / ping.0.host.127_0_0_1"
+    }
 };
